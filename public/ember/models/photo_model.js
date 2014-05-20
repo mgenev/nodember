f678c275c98982fdad5ba2eaf11b31883eab961b
Martin Genev <mgenev@gmail.com>
2014-05-20 12:29:44 -0700

    few corrections for gallery

--- public/ember/models/photo_model.js
+++ public/ember/models/photo_model.js
@@ -1,5 +1,8 @@
-  // Article
+   // Article
   App.Photo = DS.Model.extend({
-    name: DS.attr('string'),
-    path: DS.attr('string')
+      name: DS.attr('string'),
+      path: DS.attr('string'),
+      src: function() {
+          return this.get('path') + this.get('name');
+      }.property('path', 'name')
   });
