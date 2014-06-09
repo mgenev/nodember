15b94bbcf7d192dbc8b93c07a850584ad4d894b7
mgenev <mgenev@gmail.com>
2014-06-08 23:12:27 -0700

    single photo viewer improvements

--- public/ember/views/photoViewer_view.js
+++ public/ember/views/photoViewer_view.js
@@ -3,11 +3,13 @@ App.PhotoViewerView = Ember.View.extend({
       var self = this;
        $('.photo').imagesLoaded(function() { 
          self.setSidePanelHeight();
+         $('.full-size-photo-viewer').css('opacity', 1);
        });
     },
      //Em.run.scheduleOnce('afterRender', this, this.setSidePanelHeight)      
     setSidePanelHeight: function () {
       var height = $('.photo').height();
+      console.log('height is', height);
       $('.side-panel').height(height);
     }
 });
