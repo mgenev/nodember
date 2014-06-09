1e8f9ac37e87a9a3ac483f019e3d05575737041d
mgenev <mgenev@gmail.com>
2014-06-09 08:42:33 -0700

    resize added for photo viewer

--- public/ember/views/photoViewer_view.js
+++ public/ember/views/photoViewer_view.js
@@ -1,15 +1,19 @@
 App.PhotoViewerView = Ember.View.extend({
     didInsertElement: function() {
-      var self = this;
-       $('.photo').imagesLoaded(function() { 
-         self.setSidePanelHeight();
-         $('.full-size-photo-viewer').css('opacity', 1);
-       });
+        var self = this;
+        $('.photo').imagesLoaded(function() {
+            self.setSidePanelHeight();
+            $('.full-size-photo-viewer').css('opacity', 1);
+        });
+
+        $(window).resize(function() {
+          self.setSidePanelHeight();
+        });
     },
-     //Em.run.scheduleOnce('afterRender', this, this.setSidePanelHeight)      
-    setSidePanelHeight: function () {
-      var height = $('.photo').height();
-      console.log('height is', height);
-      $('.side-panel').height(height);
+    //Em.run.scheduleOnce('afterRender', this, this.setSidePanelHeight)      
+    setSidePanelHeight: function() {
+        var height = $('.photo').height();
+        console.log('height is', height);
+        $('.side-panel').height(height);
     }
 });
